da736f91c8827708616ab5246162cbba2080fd4f
ajlevy <AJLevy125@icloud.com>
2025-01-03 17:30:39 -0500

    finished

--- hashmap2.js
+++ hashmap2.js
@@ -40,6 +40,7 @@ class HashMap{
             }
             LList.append(newNode)
         }
+        this.count++
         this.check()
     }
     get(key) {
@@ -59,56 +60,116 @@ class HashMap{
     has(key){
         const index = this.hash(key);
         const LList = this.buckets[index];
-        let found = false
-        if (LList){
+        console.log(index);
+        console.log(LList.toString()); 
+        console.log(LList.head())
+        if (LList && LList.head()) { 
             let current = LList.head();
-            while (current){
-                if (current.data.key === key){
-                    found = true;
-                    return found
+            while (current) {
+                console.log(current);
+                if (current.data && current.data.key === key) {
+                    return true;
                 }
                 current = current.nextNode;
-
             }
-            return found
-        }
-        return false
-    }
-    remove(key) {
-        const index = this.hash(key);
-        const LList = this.buckets[index];
-        const current = LList.head();
-        console.log(LList.head())
-        let prev = null;
-    
-        while (current !== null) {
-            if (current.data && current.data.key === key) { // Check if data exists
-                if (prev === null) {
-                    LList.head = current.nextNode;
-                } else {
-                    prev.nextNode = current.nextNode;
-                }
-                if (this.count) {
-                    this.count -= 1;
-                }
-                return true;
-            }
-            prev = current;
-            current = current.nextNode;
         }
         return false;
     }
-    length(){}
-    clear(){}
-    keys(){}
-    value(){}
-    entries(){}
-    resize(){}
+    remove(key) {
+        if (this.has(key)) {
+            const index = this.hash(key);
+            const LList = this.buckets[index];
+            
+            if (LList && LList.head()) { // Check if LList exists and has nodes
+                let current = LList.head();
+                let prev = null;
+    
+                while (current) {
+                    if (current.data && current.data.key === key) {
+                        // If the node to remove is the head
+                        if (!prev) {
+                            LList.head = current.nextNode; // Assuming there's a setter for head
+                        } else {
+                            prev.nextNode = current.nextNode;
+                        }
+                        this.count--;
+                        this.check(); // Check if resizing is needed after removal
+                        return;
+                    }
+                    prev = current;
+                    current = current.nextNode;
+                }
+            }
+        }
+        // Key not found or bucket is empty, do nothing or handle accordingly
+    }
+    length() {
+        return this.count;
+    }
+    
+    clear() {
+        this.buckets = Array(this.size).fill().map(() => new LinkedList());
+        this.count = 0;
+        this.loadFactor = 0;
+    }
+    
+    keys() {
+        let keys = [];
+        this.buckets.forEach(bucket => {
+            let current = bucket.head();
+            while (current) {
+                keys.push(current.data.key);
+                current = current.nextNode;
+            }
+        });
+        return keys;
+    }
+    
+    values() { // Note: you had 'value()' which seems like a typo for 'values'
+        let values = [];
+        this.buckets.forEach(bucket => {
+            let current = bucket.head();
+            while (current) {
+                values.push(current.data.value);
+                current = current.nextNode;
+            }
+        });
+        return values;
+    }
+    
+    entries() {
+        let entries = [];
+        this.buckets.forEach(bucket => {
+            let current = bucket.head();
+            while (current) {
+                entries.push([current.data.key, current.data.value]);
+                current = current.nextNode;
+            }
+        });
+        return entries;
+    }
+    
+    resize() {
+        const newSize = this.size * 2;
+        const oldBuckets = this.buckets;
+        this.size = newSize;
+        this.buckets = Array(this.size).fill().map(() => new LinkedList());
+        this.count = 0;
+        oldBuckets.forEach(bucket => {
+            let current = bucket.head();
+            while (current) {
+                this.set(current.data.key, current.data.value);
+                current = current.nextNode;
+            }
+        });
+        this.check();
+    }
 }
 
-let firstHashMap = new HashMap()
-console.log(firstHashMap)
 
+
+
+/* 
 const nodeOne = new Node("1")
 const nodeTwo = new Node("2")
 const nodeThree = new Node("3")
@@ -117,20 +178,54 @@ firstHashMap.buckets[0].append(nodeOne)
 firstHashMap.buckets[0].append(nodeThree)
 firstHashMap.buckets[0].prepend(nodeTwo)
 
+
+console.log(firstHashMap.has("Four"))
 console.log(firstHashMap.hash("Four"))
 firstHashMap.set("Four", 5)
 
 console.log(firstHashMap.hash("Jimeny"))
 firstHashMap.set("Jimeny","Crickets")
 
-console.log(firstHashMap.has("Four"))
+
 console.log(firstHashMap.remove("Jimeny"))
 console.log(firstHashMap.remove("Four"))
 
-console.log(firstHashMap.has("Four"))
 
 
 
+console.log(firstHashMap.buckets[15].toString())
 
 console.log(firstHashMap.buckets[0].toString())
+console.log(firstHashMap.buckets[7].toString())
 console.log(firstHashMap)
+*/
+const test = new HashMap() // or HashMap() if using a factory
+console.log(test)
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+console.log(test.entries())
+test.set('catdog', "greenyellow")
+test.set('dolphin', 'blue')
+test.set("rotweiler", "brown")
+test.set("golden retriever", "golden")
+console.log(test.entries())
+
+test.set("moon", "silver")
+
+console.log(test.entries())
+console.log(test)
+test.set('ice cream', "black")
+test.set("catdog", "broken")
+test.set("kite", "yellow")
+test.set("golden retriever", "yellow")
+console.log(test.entries())
--- pll.js
+++ pll.js
@@ -24,6 +24,35 @@ function LinkedList(){
             List.push(Node)
         }
     }
+    function remove(value) {
+        // Check if the value exists in the list
+        if (find(value) !== null) {
+            let prevNode = null;
+            let currentNode = head();
+    
+            while (currentNode !== null) {
+                if (currentNode.data === value) {
+                    // If the node to remove is at the head
+                    if (currentNode === head()) {
+                        List.shift(); // Remove the first node from the array
+                        console.log(`removed ${JSON.stringify(currentNode.data)} at head`);
+                    } else {
+                        // Remove the node by updating the nextNode of the previous node
+                        prevNode.nextNode = currentNode.nextNode;
+                        let index = List.indexOf(currentNode);
+                        List.splice(index, 1);
+                        console.log(`removed ${JSON.stringify(currentNode.data)} inside list`);
+                    }
+                    // Exit the function after one removal if you only want to remove the first occurrence
+                    return;
+                }
+                // Move to the next node
+                prevNode = currentNode;
+                currentNode = currentNode.nextNode;
+            }
+        
+         }    
+    }
     function size(){
         return List.length
     }
@@ -71,7 +100,34 @@ function LinkedList(){
         return string
     }
 
-    return{append,prepend,at,tail,head,pop,size,contains,find,toString}
+    return{append,remove,prepend,at,tail,head,pop,size,contains,find,toString}
 }
 
 export {Node, LinkedList}
+/*
+const myLinkedList = new LinkedList()
+const One = new Node("1")
+const Two = new Node("2")
+const Three = new Node("3")
+const Four = new Node("4")
+const Five = new Node("5")
+const Six = new Node("Six")
+const Seven = new Node("7")
+const Eight = new Node("8")
+
+myLinkedList.append(Three)
+myLinkedList.prepend(Four)
+myLinkedList.append(Two)
+myLinkedList.prepend(One)
+myLinkedList.remove("2")
+console.log(myLinkedList.toString())
+
+myLinkedList.prepend(Eight)
+myLinkedList.append(Seven)
+myLinkedList.prepend(Five)
+myLinkedList.remove("3")
+
+
+console.log(myLinkedList.find("4"))
+console.log(myLinkedList.toString())
+*/
